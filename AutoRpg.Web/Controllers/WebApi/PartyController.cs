2a98c8e4360ebc9aaa0b125bf77f5a22f03f8567
nightblade9 <nightbladecodes@gmail.com>
2018-11-08 16:54:57 -0500

    Dummy party-management API call which requires authz. Verified all the workflows work as expected.

---
+++ AutoRpg.Web/Controllers/WebApi/PartyController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoRpg.Web.Controllers.WebApi
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PartyController : ControllerBase
+    {
+        private static Random random = new Random();
+
+        // GET: api/Party
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value" + random.Next(1, 100), "value" + random.Next(1, 100) };
+        }
+    }
+}
