076caf19f7e2408de50610bd7ea2f3b2917654d8
nightblade9 <nightbladecodes@gmail.com>
2018-11-08 16:37:43 -0500

    Added party service, but shared authz doesn't seem to work. IIS Express requires different ports, IIS can't find `dotnet.exe`. It's a mess.

---
+++ Microservices/AutoRpg.PartyService/Controllers/ProtectedController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoRpg.PartyService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProtectedController : ControllerBase
+    {
+        [Authorize]
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "secret value 1", "secret #2" };
+        }
+    }
+}
---
+++ Microservices/AutoRpg.PartyService/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoRpg.PartyService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private static Random r = new Random();
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {            
+            return new string[] { "value" + r.Next(0, 100), "value" + r.Next(0, 100) };
+        }
+    }
+}
